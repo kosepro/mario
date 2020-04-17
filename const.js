16045b6866a2ae9b795b0d06bb765459c115ed1f
石原康成 <yasu@one.local>
2020-04-18 03:38:13 +0900

    add new file

---
+++ const.js
@@ -0,0 +1,11 @@
+const GAME_FPS      = 1000 / 60;
+const SCREEN_SIZE_W = 256;
+const SCREEN_SIZE_H = 224;
+
+//一画面ごとのブロックの数
+const MAP_SIZE_W = SCREEN_SIZE_W/16;
+const MAP_SIZE_H = SCREEN_SIZE_H/16;
+
+//マップデータのブロックの数
+const FIELD_SIZE_W = 256;
+const FIELD_SIZE_H = 14;
